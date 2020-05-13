170df17977f11f3812f592cad3f2a8c637f3d382
paulchabz <paulwchabz@gmail.com>
2020-05-13 11:33:34 +0100

    Created a SASS file for the project updated the styles

---
+++ src/HomePageComponent/HomePageComponent.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+ export const HomePage = () => (
+    <div className="homePage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                  <h1 className="title">HATS</h1>
+                  <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">JACKETS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">TRAINERS</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">WOMEN</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">MEN</h1>
+                    <span className="subtitle">SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
