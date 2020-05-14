2ee02a088144781f1b02b15b68f2566ce4e2e944
paulchabz <paulwchabz@gmail.com>
2020-05-14 13:36:57 +0100

    Made changes to styles and menu components

---
+++ src/Directory/Directory.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import {MenuItem} from "../Menu-item/MenuItemComponent"
+import "./Directory-styles.scss"
+
+class Directory extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            section: [
+
+            {
+                title: 'hats',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id: 1,
+                linkUrl: 'hats'
+            },
+            {
+                title: 'jackets',
+                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                id: 2,
+                linkUrl: ''
+            },
+            {
+                title: 'sneakers',
+                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                id: 3,
+                linkUrl: ''
+            },
+            {
+                title: 'womens',
+                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                size: 'large',
+                id: 4,
+                linkUrl: ''
+            },
+            {
+                title: 'mens',
+                imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                size: 'large',
+                id: 5,
+                linkUrl: '',
+
+            }
+
+            ]
+        }
+    }
+    render () {
+        /*
+      return (
+          <div className="directory-menu">
+              {this.state.section.map(sections => (<MenuItem key={sections.id} title={sections.title} imageUrl={sections.imageUrl}/>))}
+          </div>
+      )
+      *
+         */
+        return (
+            <div className="directory-menu">
+                {this.state.section.map(({title, imageUrl, id, size}) =>(<MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>))}
+            </div>
+        )
+    }
+
+}
+
+export default Directory;
--- src/HomePageComponent/HomePageComponent.jsx
+++ src/HomePageComponent/HomePageComponent.jsx
@@ -1,40 +1,11 @@
 import React from "react";
 import "./HomePageComponent-styles.scss";
+import Directory from "../Directory/Directory"
 
  export const HomePage = () => (
-    <div className="homePage">
-        <div className="directory-menu">
-            <div className="menu-item">
-                <div className="content">
-                  <h1 className="title">HATS</h1>
-                  <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">JACKETS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">TRAINERS</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">WOMEN</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className="title">MEN</h1>
-                    <span className="subtitle">SHOP NOW</span>
-                </div>
-            </div>
-        </div>
-    </div>
+     <div className="homePage">
+         <Directory />
+     </div>
+
 )
 
