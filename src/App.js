4823a97ef42c07e7a99b91a1f32ef37bc478a9d4
paulchabz <paulwchabz@gmail.com>
2020-05-18 17:05:12 +0100

    Updated the styles and included a logo and header for the component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import HomePage from "./HomePageComponent/HomePageComponent"
 import ShopPage from "./Shop/ShopComponent"
 import { Switch, Route } from "react-router-dom"
+import { Header } from "./Header/HeaderComponent"
 
 
 
 function App () {
     return (
         <div>
+            <Header />
             <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route path="/shop" component={ShopPage} />
