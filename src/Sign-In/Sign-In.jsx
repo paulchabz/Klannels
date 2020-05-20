8fec94fc6ed976fa3ec876b83577b062a2a81ad9
paulchabz <paulwchabz@gmail.com>
2020-05-20 13:58:18 +0100

    created a form component for the app

---
+++ src/Sign-In/Sign-In.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Sign-In-styles.scss";
+
+class SignIn extends React.Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            email: "",
+            password: ""
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({email: "", password: ""})
+    }
+
+    handleChange = event => {
+        const { value, name } = event.target;
+        this.setState({[name]: value})
+    }
+
+   render () {
+        return (
+            <div className="sign-in">
+                <h2>I already have an account</h2>
+                <span>Please sign in with your Email and Password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input name="email" type="email" value={this.state.email} onChange={this.handleChange} required/>
+
+                    <label>Email</label>
+                    <input name="password" type="password" value={this.state.password} onChange={this.handleChange} required/>
+                    <label>Email</label>
+                    <input type="submit" value="Submit Form" />
+                </form>
+            </div>
+        )
+   }
+
+
+}
+export default SignIn;
--- src/Sign-Up-Sign-In-Forms/SignComponent.jsx
+++ src/Sign-Up-Sign-In-Forms/SignComponent.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import SignIn from "../Sign-In/Sign-In";
 import "./SignComponent-styles.scss";
 
 export const SignInSignOut = () => (
     <div className="signInsignOut">
-        SIGN IN
+        <SignIn />
     </div>
 )
